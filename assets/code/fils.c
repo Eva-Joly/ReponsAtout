4e2e88c96c7e19ab78fdefedfc5c38c529a172cc
Eva-Joly <74704192+Eva-Joly@users.noreply.github.com>
2020-12-02 16:34:20 +0100

    itteration 1 ok

--- assets/code/fils.c
+++ assets/code/fils.c
@@ -5,46 +5,107 @@
 #include<stdlib.h>
 #include<signal.h>
 #include<fcntl.h>
+#include<string.h>
 #include <string.h>
 #include <stdio.h>
 
-int lire(char*chaine, int longueur);
+//int lire(char*chaine, int longueur);
+
 
 int main(int argc, char *arg[])
 {
     int dp;
-    char commande[100];
-    char *buff[100];
+    char choix;
+    char question[250];
+    char Cpid[5];
+    char fermeture[250] = "Fermeture du client ";
+    int status = 0;
+    int boucle = 1;
+    int nbcarecrit;
 
-    printf("tapez votre commande :\n");
-    lire(commande,100);
+    status = getpid();
+    sprintf(Cpid,"%d",status);		//Converti le pid en string pour ecrire dans le pipe
 
-    sleep(8);
-    dp=open(arg[0],O_WRONLY);
-    write(dp, commande,100) ;
-  
-    close(dp);
+	dp=open(arg[1],O_WRONLY);	// On ecrit la question dans le pipe
+	
+	while(1)
+	{
+		printf("\n\tBienvenue sur notre service ReponsAtout !!!");
+		printf("\n\nTaper une question............: 1\n");
+		printf("Quitter.......................: 0\n");
+		printf("votre choix: ");
+		rewind(stdin);
+		scanf("%c",&choix);
+		getchar();
+		switch(choix)
+		{		
+			
+			case '0' :
+				strcat(fermeture,Cpid);
+				write(dp, fermeture,250);
+				close(dp);	// On ferme le pipe
+				printf("Fermeture du pipe\n");
+				while(boucle == 1)
+				{
+					printf("Appuyer sur CTR+D pour fermer le fils\n");
+					char buffer[1];
+					
+					if(fgets(buffer,10,stdin) == NULL)
+					{
+						boucle = 0;
+					}
+					else
+					{
+						printf("Veuillez saisir la bonne commande afin de quitter\n");
+					}
+				}
+	
+				printf("Fermeture du fils\n");
+				exit(0);
+				break;
+				
+			case '1' :
+				printf("tapez votre question :\n");
+				//lire(question,250);
+				
+				scanf(" %[^\n]",question);
+				getchar();
+				
+				strcat(question,"\t");
+				strcat(question,Cpid);		// On ajoute le pid a fin de la question 
 
-  exit(0);
+				dp=open(arg[1],O_WRONLY);
+				nbcarecrit = write(dp, question,sizeof(question));
+				
+				sleep(1);
+				close(dp);
+				break;
+				
+			default :
+				printf("Mauvaise commande, veuillez saisir une nouvelle commande\n");
+				break;
+	
+		}
+	}		
 }
 
-int lire(char *chaine, int longueur)
-{
-    char *positionEntree = NULL;
+//int lire(char *chaine, int longueur)
+//{
+    //char *positionEntree = NULL;
 
-    // On lit le texte saisi au clavier
-    if (fgets(chaine, longueur, stdin) != NULL)  // Pas d'erreur de saisie ?
-    {
-        positionEntree = strchr(chaine, '\n'); // On recherche l'"Entrée"
-        if (positionEntree != NULL) // Si on a trouvé le retour à la ligne
-        {
-            *positionEntree = '\0'; // On remplace ce caractère par \0
-        }
-        return 1; // On renvoie 1 si la fonction s'est déroulée sans erreur
-    }
-    else
-    {
-        return 0; // On renvoie 0 s'il y a eu une erreur
-    }
-}
+    //// On lit le texte saisi au clavier
+    //if (fgets(chaine, longueur, stdin) != NULL)  // Pas d'erreur de saisie ?
+    //{
+        //positionEntree = strchr(chaine, '\n'); // On recherche l'"Entrée"
+        //if (positionEntree != NULL) // Si on a trouvé le retour à la ligne
+        //{
+            //*positionEntree = '\0'; // On remplace ce caractère par \0
+        //}
+        //return 1; // On renvoie 1 si la fonction s'est déroulée sans erreur
+    //}
+    //else
+    //{
+  ////     return 0; // On renvoie 0 s'il y a eu une erreur
 
+    //}
+//}
--- assets/code/pere.c
+++ assets/code/pere.c
@@ -4,37 +4,128 @@
 #include<unistd.h>
 #include<stdlib.h>
 #include<signal.h>
-#include <fcntl.h>
-#include <string.h>
+#include<fcntl.h>
+#include<string.h>
+#include<sys/stat.h>
+
+#define TAILLE 250
+
+int inter = 0;
+int restart(void);
+void reponse(char buf[TAILLE]);
+void interupt();
+
 
 int main(void)
 {
-    int pid,desc,nb, pidClient;
-    char buf[80];
+    int pid,desc,nb;
+    char buf[TAILLE];
     char *argv[2];
-   
+    int boucle;
+
+    signal(SIGCHLD,interupt);
     argv[0]="pipe";
 
     unlink(argv[0]);
     mkfifo(argv[0],0666);
     
-    pid=fork();
+ do
+ {   
+   pid=fork();
     
     if(pid == 0)
     {
 	argv[1]=NULL;
-        execv("FILS", argv);
+//      execv("FILS", argv);
+        execl("/usr/bin/xterm","xterm","-e" ,"./fils",argv[0],argv[1],"set","-o","ingnoreeof",NULL);
     }
-    else{
-    	desc=open(argv[0],O_RDONLY);
-    	nb=read(desc,buf,80);
-    	buf[nb]='\0';
-    	printf(" message: %s\n ",buf);
-    	close(desc);
-    	wait(NULL);
+    else
+    {
+		inter = 0;
+		desc=open(argv[0],O_RDONLY);
+		while(inter == 0)
+		{
+
+			nb=read(desc,buf,TAILLE);
+			if(nb != 0)
+			{
+				printf("nb lu : %d\n",nb);
+				buf[nb]='\0';
+			
+				printf("Lecture : %s \n",buf);
+				if(strstr(buf,"Fermeture") == NULL)
+				{
+					reponse(buf);
+				}
+			}
+			
+		}
+		wait(NULL);
+	
+	boucle = restart();
     }
-  exit(0);
+}while (boucle == 1);
+close(desc);
+printf("Fermeture du serveur\n");
+return 0;
 }
 
+int restart(void)
+{
+	char lettre;
+	int boucle = 0;
+	do
+	{
+		printf("Voulez creer un nouveau client? Y ou N\n");
+		scanf(" %c",&lettre);
+	
+		switch(lettre)
+		{
+			case 'y' :
+				return 1;
+				break;
+				
+			case 'Y' :
+				return 1;
+				break;
+				
+			case 'N' :
+				return 0;
+				break;
+				
+			case 'n' :
+				return 0;
+				break;
+				
+			default :
+				printf("Mauvaise comande, veuillez recommencer\n");
+				break;
+		}
+	}while(boucle == 0);
+}
 
+void reponse(char buf[TAILLE])
+{
+
+	 if(strstr(buf,"temps")!=NULL || strstr(buf,"TEMPS")!=NULL || strstr(buf,"duree")!=NULL || strstr(buf,"DUREE")!=NULL)					// Compare si un mot cle correspond 
+		{
+			printf("Il vous faudra 30 min pour vous rendre à votre domicile car la circulation est dense.\n");
+
+		}
+		else if(strstr(buf,"meteo")!=NULL || strstr(buf,"METEO")!=NULL)
+		{
+			printf("Aujourd'hui le ciel est dégagé, la temprature maximale est de 24° et la minimale est à 2°\n");
+
+		}
+		else
+		{
+			printf("Veuillez nous excuser, nous ne pouvons pas répondre à votre question\n");
+		}
+
+}
+
+void interupt()
+{
+	inter = 1;
+}
 
